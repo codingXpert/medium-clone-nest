a25f8304c6a80abaac10d84bdb36a7eb1d2c0a62
Vivek Raj <vivek.raj@brainvire.com>
2023-02-02 15:51:35 +0530

    Preparing register request

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from '@app/app.service';
 import { TagModule } from '@app/tag/tag.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import ormconfig from './ormconfig'
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig) , TagModule],
+  imports: [TypeOrmModule.forRoot(ormconfig) , TagModule , UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
